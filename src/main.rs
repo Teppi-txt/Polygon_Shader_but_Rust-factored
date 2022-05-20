523115b200695060f4acfe43e953331813925ca6
Teppi Le <teppipersonal@gmail.com>
2022-05-21 00:08:38 +0700

    generation of shapes, outputted to the output/output.png file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io::Cursor;
 use std::env;
 use image::{imageops::FilterType, ImageFormat, io::Reader, open};
-use imageproc::drawing::{draw_cross_mut, draw_filled_ellipse_mut, draw_filled_circle_mut};
+use imageproc::rect::Rect;
+use imageproc::point::Point;
+use imageproc::drawing::{draw_cross_mut, draw_filled_ellipse_mut, draw_filled_circle_mut, draw_filled_rect_mut, draw_polygon_mut};
 use std::path::PathBuf;
 mod shape_gen;
-
 
 fn main() {
     let target_image = open("src/input/img.jpg").unwrap();
@@ -15,17 +16,39 @@ fn main() {
     let mut imgbuf = image::ImageBuffer::new(img_x as u32, img_y as u32);
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = image::Rgb([255 as u8, 225 as u8, 255 as u8]);
+        *pixel = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
     }
+    imgbuf.save("src/output/output.png").unwrap();
 
+    /* for i in 0..100{
+        get_best_shape(5, 10, 10);
+    } */
+    get_best_shape(50, 10, 10);
+}
 
-    for i in 0..100{
-        let shape_data = shape_gen::generate_shape_data(0, img_x, img_y);
+fn get_best_shape(shapes_per_gen: i32, child_n: i32, shapes_survive: i32) {
+    let mut image = open("src/output/output.png").unwrap();
+    let width = image.width() as i32; let height = image.height() as i32;
+
+    for shape in 0..=shapes_per_gen {
+        let mut comparison_image = open("src/output/output.png").unwrap().clone();
+        let shape_type = shape_gen::generate_shape_type();
+        let shape_data = shape_gen::generate_shape_data(shape_type, width, height);
         let shape_color = shape_gen::generate_shape_color();
-        println!("{:#?}", shape_data);
-        draw_filled_circle_mut(&mut imgbuf, shape_data[0], shape_data[1].0, image::Rgb(shape_color));
+
+        match shape_type {
+            0 => draw_filled_circle_mut(&mut comparison_image, shape_data[0], shape_data[1].0, image::Rgba(shape_color)),
+            1 => draw_filled_rect_mut(&mut comparison_image, Rect::at(shape_data[0].0, shape_data[0].1).of_size(shape_data[1].0 as u32, shape_data[1].1 as u32), image::Rgba(shape_color)),
+            2 => {
+                let mut poly_list = Vec::new();
+                for point in shape_data {
+                    poly_list.push(Point::new(point.0, point.1));
+                }
+                draw_polygon_mut(&mut comparison_image, &poly_list, image::Rgba(shape_color));
+            }
+            _ => panic!("shape type returned an unexpected value!?"),
+        }
+        comparison_image.save("src/output/test.png").unwrap();
         println!{"done"};
     }
-    
-    imgbuf.save("src/output/woah.png").unwrap();
 }
--- src/shape_gen.rs
+++ src/shape_gen.rs
@@ -1,18 +1,42 @@
+/* shape map:
+0 - circle
+1 - rect
+2 - polygon
+3 - cross
+4 - line
+ */
+
 pub fn generate_shape_data(shape_type: i32, width: i32, height: i32) -> Vec<(i32, i32)>{
     use rand::{thread_rng, Rng};
     //circle data stored as follows: "circle", "circle width and height", "center"
     let mut rng = thread_rng();
-    return vec![(rng.gen_range(0..=width), rng.gen_range(0..=height)), (rng.gen_range(0..=width/2), rng.gen_range(0..=height/2))];
+
+    match shape_type {
+        //[(center_x, center_y), (radius_x, radius_y)]
+        0 => return vec![(rng.gen_range(0..=width), rng.gen_range(0..=height)), (rng.gen_range(0..=width/2), rng.gen_range(0..=height/2))],
+
+        //[(start_x, start_y), (width, width_y)]
+        1 => return vec![(rng.gen_range(0..=width), rng.gen_range(0..=height)), (rng.gen_range(0..=width), rng.gen_range(0..=height))],
+
+        2 => {
+            let mut poly_list = Vec::new();
+            for point in 1..=rng.gen_range(3..=4) {
+                poly_list.push((rng.gen_range(0..=width), rng.gen_range(0..=height)));
+            }
+            return poly_list;
+        }
+        _ => panic!("Gen range returned an unexpected value!?"),
+    }
 }
 
 pub fn generate_shape_type() -> i32 {
     use rand::{thread_rng, Rng};
-
-    let shape_type = 0;//rng.gen_range(0..=0);
-    return shape_type;
+    let mut rng = thread_rng();
+    return rng.gen_range(0..=2);
 }
 
-pub fn generate_shape_color() -> [u8, u8 ,u8] {
+pub fn generate_shape_color() -> [u8; 4] {
     use rand::{thread_rng, Rng};
-    return [rng.gen_range(0..=255), rng.gen_range(0..=255), rng.gen_range(0..=255)];
+    let mut rng = thread_rng();
+    return [rng.gen_range(0..=255), rng.gen_range(0..=255), rng.gen_range(0..=255), rng.gen_range(0..=255)];
 }
